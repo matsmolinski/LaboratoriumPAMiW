2503c67920c94e12249a01450bf6f43673c78444
matsmolinski <matsmolinski@gmail.com>
2019-11-23 14:43:37 +0100

    Added session management

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,23 @@
 from flask import Flask, Blueprint, request, Response, render_template
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+import os
 import redis
 import json
+import random
+import string
+
+def generateKey(stringLength):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for i in range(stringLength))
 
 app = Flask(__name__)
 CORS(app)
 db = redis.Redis(host = 'redis', port = 6379, decode_responses=True)
+
+app.config["JWT_SECRET_KEY"] = generateKey(20)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 300
+jwt = JWTManager(app)
 
 def isLoginInDatabase(login):
     users = []
@@ -53,8 +65,6 @@ def addUser(login, email, password):
 # Index
 @app.route('/database', methods=['GET'])
 def process_data():
-    db.set("slawek", 30)
-    db.expire("slawek", 60)
     return Response("Why do you use get?", 200)
 
 @app.route('/database', methods=['POST'])
@@ -70,17 +80,37 @@ def tryToAddLogin():
         addUser(login, email, password)
         return Response("User " + login + " successfully added", 200)
 
-@app.route('/authorise', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def tryToLogIn():
     user = json.loads(request.data)
     if isLoginInDatabase(user["name"]):
         if checkPassword(user):
-            return Response("User logged in", 200)
+            sessionid = generateKey(10)
+            db.set(sessionid, 1, ex = 300)
+            access_token = create_access_token(identity = user["name"])
+            message = {
+                "sessionid": sessionid,
+                "jwt": access_token
+            }
+            return Response(json.dumps(message), 200)
+            
         else:
             return Response("Password is invalid", 211)
     else:
         return Response("User unrecognized", 210)
 
-#return 'PAMIW >> Hello World'
+@app.route('/logout', methods=['POST'])
+def tryToLogOut():
+    user = json.loads(request.data)
+    db.delete(user["sessionid"])
+    return Response("Logged out", 200)
+
+@app.route('/check', methods=['POST'])
+def checkIfLoggedIn():
+    session = db.get(request.data)
+    if session == None:
+        return Response("Authorization failed", 201)
+    return Response("Everything fine", 200)
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
--- frontend/static/cloud.js
+++ frontend/static/cloud.js
@@ -1,3 +1,5 @@
+
+
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
@@ -25,50 +27,39 @@ function checkSession() {
         Http.send(sid);
     });
     promise.then((value) => {
-        if(value[1] == 200) {
-            window.location.replace("http://localhost:3000/cloud");
+        if(value[1] !== 200) {
+            alert('You are not authorized to use this site');
+            window.location.replace("http://localhost:3000/login");
         }
     }).catch((message) => {
         document.getElementById('error').innerHTML = message;
     });
 }
 
+
 function tryToSubmit() {
-    let username = document.getElementById('username').value;
-    let passwd = document.getElementById('passwd').value;
+
     var user = {
-        name: username,
-        password: passwd
+        name: getCookie("user"),
+        sessionid: getCookie("sessionid")
     }
-    user = JSON.stringify(user)
     const promise = new Promise((resolve, reject) => {
         const Http = new XMLHttpRequest();
-        const url='http://localhost:3030/login';
+        const url='http://localhost:3030/logout';
         Http.open("POST", url);
         Http.onload = () => resolve([Http.response, Http.status]);
         Http.onerror = () => reject(Http.statusText);
         Http.send(user);
     });
     promise.then((value) => {
-        if(value[1] === 200) {
-            let resp = JSON.parse(value[0]);
-            /*let time = Date.now();
-            let date = new Date();
-            time += 300 * 1000;
-            date.setTime(time);*/
-            document.cookie = 'sessionid=' + resp.sessionid + '; path=/;"';
-            document.cookie = 'jwt=' + resp.jwt + '; path=/;"';
-            //document.cookie = 'user=' + username + '; expires=' + date.toUTCString() + '; path=/;"';
-            window.location.replace("http://localhost:3000/cloud");
-        }
-        else {
-            document.getElementById('error-username').innerHTML = value[0];
-        }
+        document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie = "sessionid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        window.location.replace("http://localhost:3000/login");
     }).catch((message) => {
-        document.getElementById('error-username').innerHTML = message;
+        document.getElementById('error').innerHTML = message;
     });
 
-    return false;
 }
+
 
 window.onload = checkSession;
