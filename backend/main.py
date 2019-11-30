dac5f5b652bfbe86a552a8764924846b9bcc16ec
matsmolinski <matsmolinski@gmail.com>
2019-11-30 16:24:36 +0100

    working version without heroku

--- backend/main.py
+++ backend/main.py
@@ -20,54 +20,7 @@ db = redis.Redis(host = 'redis', port = 6379, decode_responses=True)
 app.config["JWT_SECRET_KEY"] = generateKey(20)
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 300
 jwt = JWTManager(app)
-
-def isLoginInDatabase(login):
-    users = []
-    try:
-        file = open("database.txt", 'r')
-        for line in file:
-            line = line.strip().split()
-            users.append({
-                'login': line[0],
-                'email': line[1],
-                'password': line[2]
-            })
-        for user in users:
-            if(user["login"] == login):
-                return True
-        return False
-    except IOError:
-        return False
-
-def checkPassword(luser):
-    users = []
-    try:
-        file = open("database.txt", 'r')
-        for line in file:
-            line = line.strip().split()
-            users.append({
-                'login': line[0],
-                'email': line[1],
-                'password': line[2]
-            })
-        for user in users:
-            if(user["login"] == luser['name']):
-                if user["password"] == luser['password']:
-                    return True
-                return False
-                
-        return False
-    except IOError:
-        return False
-
-def addUser(login, email, password):
-    file = open("database.txt", 'a')
-    file.write(login + ' ' + email + ' ' + password + '\n')
-
-@app.route("/static/<path:path>")
-def send_static(path):
-    return send_from_directory('static', path)
-
+ 
 SWAGGER_URL = "/swagger"
 API_URL = "/static/swagger.json"
 swaggerui_blueprint = get_swaggerui_blueprint(
@@ -81,50 +34,87 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 
+def addUser(login, email, password):
+    db.hset(login, "email", email)
+    db.hset(login, "password", password)
+
+def isLoginInDatabase(login):
+    email = db.hget(login, 'email')
+    if email == None:
+        return False
+    return True
+
+def checkPassword(user):
+    if db.hget(user["name"], "password") == user['password']:
+        return True
+    return False
+    
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory('static', path)
+
 @app.route('/database', methods=['POST'])
 def tryToAddLogin():
-    user = json.loads(request.data)
-    login = user['name']
-    email = user['email']
-    password = user['password']
+    try:
+        user = json.loads(request.data)
+        login = user['name']
+        email = user['email']
+        password = user['password']
 
-    if isLoginInDatabase(login):
-        return Response("Login " + login + " is already in use!", 201)
-    else:
-        addUser(login, email, password)
-        return Response("User " + login + " successfully added", 200)
+        if isLoginInDatabase(login):
+            return Response("Login " + login + " is already in use!", 201)
+        else:
+            addUser(login, email, password)
+            return Response("User " + login + " successfully added", 200)
+    except Exception as e:
+        print(e, file = sys.stderr)
+        return Response("Failed to read request", 400)
 
 @app.route('/login', methods=['POST'])
 def tryToLogIn():
-    user = json.loads(request.data)
-    if isLoginInDatabase(user["name"]):
-        if checkPassword(user):
-            sessionid = generateKey(10)
-            db.set(sessionid, 1, ex = 300)
-            access_token = create_access_token(identity = user["name"])
-            message = {
-                "sessionid": sessionid,
-                "jwt": access_token
-            }
-            return Response(json.dumps(message), 200)
-            
+    try:
+        user = json.loads(request.data)
+        if isLoginInDatabase(user["name"]):
+            if checkPassword(user):
+                sessionid = generateKey(10)
+                db.set(sessionid, 1, ex = 300)
+                access_token = create_access_token(identity = user["name"])
+                message = {
+                    "sessionid": sessionid,
+                    "jwt": access_token
+                }
+                return Response(json.dumps(message), 200)
+                
+            else:
+                return Response("Password is invalid", 211)
         else:
-            return Response("Password is invalid", 211)
-    else:
-        return Response("User unrecognized", 210)
+            return Response("User unrecognized", 210)
+    except Exception as e:
+        print(e, file = sys.stderr)
+        return Response("Failed to read request", 400)
+    
 
 @app.route('/logout', methods=['POST'])
 def tryToLogOut():
-    user = json.loads(request.data)
-    db.delete(user["sessionid"])
-    return Response("Logged out", 200)
+    try:
+        user = json.loads(request.data)
+        db.delete(user["sessionid"])
+        return Response("Logged out", 200)
+    except Exception as e:
+        print(e, file = sys.stderr)
+        return Response("Failed to read request", 400)
+    
 
 @app.route('/check', methods=['POST'])
 def checkIfLoggedIn():
-    session = db.get(request.data)
-    if session == None:
-        return Response("Authorization failed", 201)
-    return Response("Everything fine", 200)
+    try:
+        session = db.get(request.data)
+        if session == None:
+            return Response("Authorization failed", 201)
+        return Response("Everything fine", 200)
+    except Exception as e:
+        print(e, file = sys.stderr)
+        return Response("Failed to read request", 400)
 
 @app.route('/pdfs/list', methods=['GET'])
 def getPdfList():
@@ -136,14 +126,17 @@ def getPdfList():
 
 @app.route("/pdfs", methods=["POST"])
 def uploadPdf():
-    f = request.files["pdf"]
-    savePdf(f)
-    return redirect("http://localhost:3000/cloud")
+    try:
+        f = request.files["pdf"]
+        savePdf(f)
+        return redirect("http://localhost:3000/cloud")
+    except Exception as e:
+        print(e, file = sys.stderr)
+        return Response("Failed to read request", 400)
 
 @app.route("/pdfs/<string:name>", methods=["GET"])
 @jwt_required
 def downloadPdf(name):
-    print(request.cookies.get("jwt"))
     full_name = db.hget(name, "path_to_file")
     org_filename = db.hget(name, "org_filename")
     if(full_name != None):
